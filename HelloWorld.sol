fd7f02f0129bd142e16d20ad639d4b17810b75e0
Gustavo <ghpo@users.noreply.github.com>
2022-07-04 17:14:10 -0300

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,73 @@
+pragma solidity 0.5.3;
+
+
+/**
+ * The contractName contract does this and that...
+ */
+contract HelloWorld {
+
+    string public text;
+    uint public number;
+    address public userAddress;
+    bool public answer;
+    mapping (address => uint) public hasInteracted;
+
+
+    function setText (string memory myText) public {
+  	text = myText;
+    setInteracted();
+    }
+  
+    function setNumber(uint myNumber) public {
+        number = myNumber; 
+        setInteracted();
+    }
+
+    function setUserAddress() public {
+        userAddress = msg.sender;
+        setInteracted();
+    }
+
+    function setAnswer(bool trueOrFalse) public {
+        answer = trueOrFalse;
+        setInteracted();
+    }
+
+    function setInteracted() private {
+        hasInteracted[msg.sender] += 1;
+    }
+
+    function sum(uint num1, uint num2) public pure returns(uint) {
+        return num1 + num2;
+    }
+
+    function sub(uint num1, uint num2) public pure returns(uint) {
+        return num1 - num2;
+    }
+
+    function div(uint num1, uint num2) public pure returns(uint) {
+        return num1 / num2;
+        
+    }
+
+    function mult(uint num1, uint num2) public pure returns(uint) {
+        return num1 * num2;
+    }
+
+
+    function pow(uint num1, uint num2) public pure returns(uint) {
+        return num1 ** num2;
+    }
+
+//Explicação:
+// Funções do tipo PURE não alteram nem consultam na blockchain
+// Funções do tipo VIEW só consultam, mas não alteram coisas na blockchain
+
+    function sumStored(uint num1) public view returns(uint) {
+        return num1 + number;
+    }
+
+
+
+
+}
